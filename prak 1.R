ae952d5aea784c4d10edc3b412d66e8002b94483
Maukana Hafizh Ariputra <114157840+hafizharp@users.noreply.github.com>
2023-07-30 18:59:29 +0700

    Add files via upload

---
+++ prak 1.R
@@ -0,0 +1,6 @@
+data_hafizh = read.delim("clipboard")
+View(data_hafizh)
+mean(data_hafizh$TinggiBadan)
+str(data_hafizh)
+
+data_hafizh=read.xlsx("C:\Users\LENOVO\OneDrive\Documents\praktikum statistika\BahanPraktikum1_hafizh.csv")
